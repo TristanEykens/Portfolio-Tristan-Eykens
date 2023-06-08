8e55be7b2d719d35d30136d52f25abb3c06a2977
trist <12000589@student.pxl.be>
2023-06-08 17:03:23 +0200

    Alle pagina's aangemaakt. contact pagina bijna volledig en Hobbies pagina skelet code gemaakt.

---
+++ Hobbies.js
@@ -0,0 +1,26 @@
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundColor = 'lightblue';
+
+// trigger this function every time the user scrolls
+window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+    if (scroll < 700) {
+        // green
+        body.style.backgroundColor = 'lightblue';
+    } else if (scroll >= 700 && scroll < 1500) {
+        // yellow
+        body.style.backgroundColor = '#f9dc90';
+    } else if (scroll >= 1500 && scroll < 2500) {
+        // blue
+        body.style.backgroundColor = '#f89e9d';
+    } else if (scroll >= 2500 && scroll < 3400) {
+        // orange
+        body.style.backgroundColor = '#d46f93';
+    } else if (scroll >= 3400 && scroll < 4400) {
+        // red
+        body.style.backgroundColor = '#805690';
+    } else {
+        // purple
+        body.style.backgroundColor = '#2A3A70';
+    }
+}
--- Home.js
+++ Home.js
@@ -1,26 +1,26 @@
 var body = document.getElementsByTagName('body')[0];
-body.style.backgroundColor = 'yellow';
+body.style.backgroundColor = 'lightblue';
 
 // trigger this function every time the user scrolls
 window.onscroll = function (event) {
     var scroll = window.pageYOffset;
     if (scroll < 700) {
         // green
-        body.style.backgroundColor = 'yellow';
+        body.style.backgroundColor = 'lightblue';
     } else if (scroll >= 700 && scroll < 1500) {
         // yellow
         body.style.backgroundColor = 'lightgreen';
     } else if (scroll >= 1500 && scroll < 2500) {
         // blue
-        body.style.backgroundColor = 'lightblue';
+        body.style.backgroundColor = 'pink';
     } else if (scroll >= 2500 && scroll < 3400) {
         // orange
         body.style.backgroundColor = 'orange';
     } else if (scroll >= 3400 && scroll < 4400) {
         // red
-        body.style.backgroundColor = 'pink';
+        body.style.backgroundColor = 'purple';
     } else {
         // purple
-        body.style.backgroundColor = 'purple';
+        body.style.backgroundColor = 'darkblue';
     }
 }
