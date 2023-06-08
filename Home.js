3fb9101ab92d2759dd312100a115b1e30bffc99d
trist <12000589@student.pxl.be>
2023-06-08 11:30:51 +0200

    Update op aantrekkelijkheid Home 1

---
+++ Home.js
@@ -0,0 +1,26 @@
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundColor = 'yellow';
+
+// trigger this function every time the user scrolls
+window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+    if (scroll < 700) {
+        // green
+        body.style.backgroundColor = 'yellow';
+    } else if (scroll >= 700 && scroll < 1500) {
+        // yellow
+        body.style.backgroundColor = 'lightgreen';
+    } else if (scroll >= 1500 && scroll < 2500) {
+        // blue
+        body.style.backgroundColor = 'lightblue';
+    } else if (scroll >= 2500 && scroll < 3400) {
+        // orange
+        body.style.backgroundColor = 'orange';
+    } else if (scroll >= 3400 && scroll < 4400) {
+        // red
+        body.style.backgroundColor = 'pink';
+    } else {
+        // purple
+        body.style.backgroundColor = 'purple';
+    }
+}
