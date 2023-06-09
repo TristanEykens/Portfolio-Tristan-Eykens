260d1b650cef8f3c54def7e68fc249bdb3df2551
trist <12000589@student.pxl.be>
2023-06-09 14:25:17 +0200

    Wisseling tussen reflecties 1 & 2 mogelijk. Responsiveness verbeterd.

--- Home.js
+++ Home.js
@@ -9,18 +9,18 @@ window.onscroll = function (event) {
         body.style.backgroundColor = 'lightblue';
     } else if (scroll >= 700 && scroll < 1500) {
         // yellow
-        body.style.backgroundColor = 'lightgreen';
+        body.style.backgroundColor = '#f9dc90';
     } else if (scroll >= 1500 && scroll < 2500) {
         // blue
-        body.style.backgroundColor = 'pink';
+        body.style.backgroundColor = '#f89e9d';
     } else if (scroll >= 2500 && scroll < 3400) {
         // orange
-        body.style.backgroundColor = 'orange';
+        body.style.backgroundColor = '#d46f93';
     } else if (scroll >= 3400 && scroll < 4400) {
         // red
-        body.style.backgroundColor = 'purple';
+        body.style.backgroundColor = '#805690';
     } else {
         // purple
-        body.style.backgroundColor = 'darkblue';
+        body.style.backgroundColor = '#2A3A70';
     }
 }
