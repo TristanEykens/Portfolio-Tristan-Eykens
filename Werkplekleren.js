55e63d2079ad3f1d0d5c76607abbe48c31b40382
Tristan Eykens <73166856+TristanEykens@users.noreply.github.com>
2023-06-11 14:01:19 +0200

    Teskten en Foto's toegevoegd

--- Werkplekleren.js
+++ Werkplekleren.js
@@ -31,3 +31,23 @@ function PowerBarScroll(elementId) {
         element.scrollIntoView({behavior: 'smooth'});
     }
 }
+
+/* achtergrond kleur verandering bij de Pasta Webshop sectie*/
+$(window).scroll(function () {
+    $('body').each(function () {
+
+        var topOfWindow = $(window).scrollTop(), bottomOfWindow = topOfWindow + $(window).height();
+        var boxPos = $('#WerkpleklerenWebsiteSectie').offset().top;
+
+        /* De 500 verwijst naar hoeveel pixels van de bodem van je scherm de sectie moet zijn eer hij de kleur veranderd.
+        De 300 verwijst dan naar hoeveel pixels hij moet zijn vanaf de top van je scherm voordat hij de kleur terug zet zoals daarvoor.*/
+if(boxPos <= bottomOfWindow-500 && boxPos >= topOfWindow-300){
+    $("body").css("background-color", "#1D1D1B");
+    $("#Terug-Naar-Boven-Knop").css("background-color", "#F8B960");
+}
+else{
+    $("body").css("background-color", "#F3FAFE");
+    $("#Terug-Naar-Boven-Knop").css("background-color", "#B6A8FF");
+}
+});
+});
